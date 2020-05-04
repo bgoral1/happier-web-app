730869068892782bcd5ca0da8c7e0ec841863cce
Barbara Góral <barbara.zofia.goral@gmail.com>
2020-05-04 10:55:27 +0200

    Delete VSC directory .history from repo and add it to .gitignore

--- .history/src/components/Button/Button.stories_20200310153224.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Button from './Button';
-
-export default { title: 'Button' };
-
-export const primary = () => <Button>Szukaj</Button>;
-export const secondary = () => <Button secondary>Hello Button</Button>;
-export const tertiary = () => <Button tertiary>Hello Button</Button>;
--- .history/src/components/Button/Button_20200309175915.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const Button = styled.button`
-  background-color: #e91e63;
-`;
-
-export default Button;
