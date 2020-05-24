d9aaf7d6aeb6abfb6f8415d9e55523e3639f1cb4
Barbara Góral <barbara.zofia.goral@gmail.com>
2020-05-24 21:52:59 +0200

    Delete storybook from project

--- src/components/atoms/Button/Button.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Button from './Button';
-
-export default { title: 'Button' };
-
-export const primary = () => <Button>Szukaj</Button>;
-export const secondary = () => <Button secondary>Anuluj</Button>;
-export const tertiary = () => <Button tertiary>Edytuj</Button>;
