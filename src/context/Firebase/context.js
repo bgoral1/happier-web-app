73ad934e45117d70eb6f59836c094f34784d6a41
Barbara Góral <barbara.zofia.goral@gmail.com>
2020-06-14 16:54:12 +0200

    Delete unnecessary commented code

--- src/context/Firebase/context.js
+++ src/context/Firebase/context.js
@@ -4,7 +4,6 @@ import useAuth from 'context/Firebase/useAuth';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'theme/GlobalStyle';
 import { theme } from 'theme/MainTheme';
-// import SEO from 'components/SEO/seo';
 
 export const FirebaseContext = createContext();
 
@@ -12,7 +11,6 @@ const FirebaseContextProvider = ({ children }) => {
   const { user, firebase, loading } = useAuth();
   return (
     <FirebaseContext.Provider value={{ user, firebase, loading }}>
-      {/* <SEO /> */}
       <GlobalStyle />
       <ThemeProvider theme={theme}>{children}</ThemeProvider>
     </FirebaseContext.Provider>
