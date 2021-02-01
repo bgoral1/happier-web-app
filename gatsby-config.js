db445f5b6668a57f91ff49850c8edffd2457900d
Barbara Góral <barbara.zofia.goral@gmail.com>
2021-02-01 13:07:35 +0100

    Add config to firebase

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,9 @@
 /* eslint-disable global-require */
+
+require('dotenv').config({
+  path: '.env',
+})
+
 const path = require('path');
 
 module.exports = {
@@ -11,7 +16,18 @@ module.exports = {
     {
       resolve: 'gatsby-firesource',
       options: {
-        credential: require('./firebase.json'),
+        credential: {
+          "type": process.env.FIREBASE_TYPE,
+          "project_id": process.env.FIREBASE_PROJECT_ID,
+          "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+          "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+          "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+          "client_id": process.env.FIREBASE_CLIENT_ID,
+          "auth_uri": process.env.FIREBASE_AUTH_URI,
+          "token_uri": process.env.FIREBASE_TOKEN_URI,
+          "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+          "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+        },
         types: [
           {
             type: 'Pet',
