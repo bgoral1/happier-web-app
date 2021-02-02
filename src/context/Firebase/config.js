87383aa5373e61803dc0107e16c88c1c179a0c2f
Barbara Góral <barbara.zofia.goral@gmail.com>
2021-02-02 16:23:48 +0100

    Update config in Firebase context

--- src/context/Firebase/config.js
+++ src/context/Firebase/config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APIKEY,
-  authDomain: process.env.FIREBASE_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_SENDER_ID,
-  appId: process.env.FIREBASE_APPID,
+  apiKey: process.env.GATSBY_FIREBASE_APIKEY,
+  authDomain: process.env.GATSBY_FIREBASE_DOMAIN,
+  databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+  projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_FIREBASE_SENDER_ID,
+  appId: process.env.GATSBY_FIREBASE_APPID,
 };
 
 export default firebaseConfig;
